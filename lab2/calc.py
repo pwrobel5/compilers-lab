ce484b3c56627fed0dd0ce96d1aac00b54448a20
Piotr Wróbel <pwrobel5@gmail.com>
2020-03-31 15:08:27 +0200

    for loop, precedence fixes

--- lab2/calc.py
+++ lab2/calc.py
@@ -11,7 +11,12 @@ import math
 
 reserved = {
     'if' : 'IF',
-    'fi' : 'FI'
+    'else' : 'ELSE',
+    'while' : 'WHILE',
+    'for' : 'FOR',
+    'function' : 'CUSTOMFUNC',
+    'procedure' : 'PROCEDURE',
+    'return' : 'RETURN'
 }
 
 tokens = [
@@ -71,45 +76,59 @@ lexer = lex.lex()
 # Parsing rules
 
 precedence = (
-    ('left', 'RELATIONAL'),
+    ('nonassoc', 'IFX', 'WHILEX', 'FORX'),
+    ('nonassoc', 'ELSE'),
+    ('nonassoc', 'RELATIONAL'),
     ('left', '+', '-'),
     ('left', '*', '/'),
     ('left', 'POWER'),
     ('right', 'FUNCTION'),    
     ('left', ';'),
-    ('right', 'IF'),
     ('right', 'UMINUS')
 )
 
 # dictionary of names
 names = {}
 
-def p_statement_assign(p):
-    'statement : NAME EQUALS expression'
-    names[p[1]] = p[3]
+def p_statement_multi(p):
+    '''statement : statement ';' statement
+                 | empty'''
 
 def p_expression_ignored_eq(p):
     'expression : EQUALS_IGNORED'
     pass
 
-def p_statement_multi(p):
-    '''statement : statement ';' statement
-                 | empty'''
-
 def p_empty(p):
     'empty : '
 
 def p_statement_expr(p):
-    'statement : expression'
+    '''statement : expression
+                 | relation'''
     print(p[1])
 
 def p_statement_other(p):
-    'statement : conditional'
+    '''statement : conditional
+                 | loop
+                 | assignment'''
+
+def p_statement_assign(p):
+    'assignment : NAME EQUALS expression'
+    names[p[1]] = p[3]
 
 def p_conditional(p):
-    '''conditional : IF '(' expression ')' statement FI '''
+    '''conditional : IF '(' relation ')' statement %prec IFX
+                   | IF '(' relation ')' statement ELSE statement '''   
     if p[3]:
         p[0] = p[5]
+    elif len(p) == 8:
+        p[0] = p[7]
+
+def p_while(p):
+    '''loop : WHILE '(' relation ')' statement %prec WHILEX '''
+
+def p_for(p):
+    '''loop : FOR '(' assignment ';' relation ';' assignment ')' statement %prec FORX '''
+    pass
 
 def p_expression_binop(p):
     '''expression : expression '+' expression
@@ -138,7 +157,7 @@ def p_expression_uminus(p):
     p[0] = -p[2]
 
 def p_relation(p):
-    'expression : expression RELATIONAL expression'
+    'relation : expression RELATIONAL expression'
     if p[2] == '==':
         p[0] = p[1] == p[3]
     elif p[2] == "!=":
